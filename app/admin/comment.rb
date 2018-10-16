fd5ee3f6bb06455bc21c3231454c0ca52bab549b
zikoku07 <zikoku07@gmail.com>
2018-10-16 20:02:50 +0600

    html tag removed

--- app/admin/comment.rb
+++ app/admin/comment.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Comment do
-  permit_params :comment, :commentable, :user
+  permit_params :user_id, :comment, :commentable_type, :commentable_id, :title, :role, :comment_html
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    column (:comment) { |message| raw(message.comment) }
+    column :commentable_type
+    column :user_id
+    actions
+  end
+
+
 end
