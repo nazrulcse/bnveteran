a926467f2b29bdbd2b345a1de376e8ed4f415f60
zikoku07 <zikoku07@gmail.com>
2018-10-30 20:10:25 +0600

    after_sign_in path issue fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource.class == User
-      user_root_path
+      request.env['omniauth.origin'] || stored_location_for(resource) || user_root_path
     else
       super
     end
